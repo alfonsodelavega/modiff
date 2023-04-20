53b5e889e4063a0a63c8b28c8c8c5021de23abe5
alfonsodelavega <alfonso.delavega@unican.es>
2023-04-20 20:56:46 +0200

    Add modiff comparison over uml models (simplified)
    EMF Compare encounters differences between line-equal models.
    
    Weird.

---
+++ org.eclipse.epsilon.modiff/src/main/java/org/eclipse/epsilon/modiff/misc/CompareDifferentOrdering.java
@@ -0,0 +1,30 @@
+package org.eclipse.epsilon.modiff.misc;
+
+import java.io.IOException;
+
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.eclipse.epsilon.modiff.Modiff;
+import org.eclipse.epsilon.modiff.matcher.IdOrNameMatcher;
+import org.eclipse.uml2.uml.UMLPackage;
+
+public class CompareDifferentOrdering {
+
+	public static void main(String[] args) throws IOException {
+
+		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(
+				"*", new XMIResourceFactoryImpl());
+		ResourceSet resSet = new ResourceSetImpl();
+		resSet.getPackageRegistry().put("http://www.eclipse.org/uml2/5.0.0/UML", UMLPackage.eINSTANCE);
+
+		Modiff modiff = new Modiff(
+				"models/uml-differentOrdering/m1-mini.uml",
+				"models/uml-differentOrdering/m2-mini.uml");
+		modiff.setMatcher(new IdOrNameMatcher());
+		modiff.compare();
+
+		System.out.println("Done");
+	}
+}
