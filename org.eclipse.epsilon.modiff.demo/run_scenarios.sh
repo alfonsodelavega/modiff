7fd55996b786146f7d63f24682f592ef789b75c3
alfonsodelavega <alfonso.delavega@unican.es>
2023-09-20 19:46:27 +0200

    Add demo scenario and complete script

--- org.eclipse.epsilon.modiff.demo/run_scenarios.sh
+++ org.eclipse.epsilon.modiff.demo/run_scenarios.sh
@@ -4,7 +4,19 @@ modiff_jar=$(realpath ../org.eclipse.epsilon.modiff/target/org.eclipse.epsilon.m
 
 repairshop_metamodel=$(realpath metamodel/repairshop.ecore)
 
+declare -a scenarios=("s00" "s01")
+
 from=$(realpath scenarios/s00/s00-from.model)
 to=$(realpath scenarios/s00/s00-to.model)
 
-java -jar ${modiff_jar} -from=${from} -to=${to} -ecore=${repairshop_metamodel}
+for s in "${scenarios[@]}"
+do
+    from=$(realpath scenarios/${s}/${s}-from.model)
+    to=$(realpath scenarios/${s}/${s}-to.model)
+
+    # standard diff over xmi
+    diff -u $from $to > scenarios/${s}/${s}_xmidiff.diff
+
+    # modiff + munidiff (requires mvn clean verify on org.eclipse.epsilon.modiff)
+    java -jar ${modiff_jar} -from=${from} -to=${to} -ecore=${repairshop_metamodel} > scenarios/${s}/${s}_munidiff.diff
+done
